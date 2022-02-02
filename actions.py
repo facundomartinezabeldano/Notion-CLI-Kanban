20eb394f4f774a8a3ecc17ad626e7c1d77d54744
Facundo <57848306+facundomartinezabeldano@users.noreply.github.com>
2022-02-01 23:07:06 -0300

    I dunno man

--- actions.py
+++ actions.py
@@ -12,24 +12,43 @@ def edit_task_action(task):
     return
 
 
-def list_tasks_action():
-    query = notion.databases.query(database_id="63cd54d3b2254b02b9f258c52e38400a")['results']
+def list_tasks_action(showbar=True):
+    query = notion.databases.query(
+        database_id="63cd54d3b2254b02b9f258c52e38400a")['results']
     urls = [element['url'] for element in query]
     task_list_payload = []
     task_ids_payload = []
-    with alive_bar(len(urls)) as bar:
-        for url in urls:
-            p = notion.pages.retrieve(get_id(url))
-            r = [
-                p['properties']['Task']['title'][0]['text']['content'], #title
-                p['properties']['Short Description']['rich_text'][0]['text']['content'], #Description
-                p['properties']['Status']['multi_select'][0]['name'],   #Status
-                p['properties']['Due date']['rich_text'][0]['text']['content'], #Due date
-            ]
-            task_list_payload.append(r)
-            task_ids_payload.append(get_id(url))
-            bar()
-    return task_list_payload , task_ids_payload
+
+    if(showbar):
+        with alive_bar(len(urls)) as bar:
+            for url in urls:
+                p = notion.pages.retrieve(get_id(url))
+                r = [
+                    p['properties']['Task']['title'][0]['text']['content'],  # title
+                    # Description
+                    p['properties']['Short Description']['rich_text'][0]['text']['content'],
+                    p['properties']['Status']['multi_select'][0]['name'],  # Status
+                    # Due date
+                    p['properties']['Due date']['rich_text'][0]['text']['content'],
+                ]
+                task_list_payload.append(r)
+                task_ids_payload.append(get_id(url))
+                bar()
+            return task_list_payload, task_ids_payload
+
+    for url in urls:
+        p = notion.pages.retrieve(get_id(url))
+        r = [
+            p['properties']['Task']['title'][0]['text']['content'],  # title
+            # Description
+            p['properties']['Short Description']['rich_text'][0]['text']['content'],
+            p['properties']['Status']['multi_select'][0]['name'],  # Status
+            p['properties']['Due date']['rich_text'][0]['text']['content'],  # Due date
+        ]
+        task_list_payload.append(r)
+        task_ids_payload.append(get_id(url))
+
+    return task_list_payload, task_ids_payload
 
 
 def delete_task_action(id):
@@ -41,47 +60,48 @@ def set_status_action(task_id, status):
     return
 
 
-def add_task_action(request):
+def add_task_action(request) -> None:
     payload = {'Due date': {'id': '~ePg',
-                              'rich_text': [{'annotations': {'bold': False,
-                                                             'code': False,
-                                                             'color': 'default',
-                                                             'italic': False,
-                                                             'strikethrough': False,
-                                                             'underline': False},
-                                             'href': None,
-                                             'plain_text': request['Due date'],
-                                             'text': {'content': request['Due date'],'link': None},
-                                             'type': 'text'}],
-                              'type': 'rich_text'},
-                 'Short Description': {'id': 'DEgC',
-                                       'rich_text': [{'annotations': {'bold': False,
-                                                                      'code': False,
-                                                                      'color': 'default',
-                                                                      'italic': False,
-                                                                      'strikethrough': False,
-                                                                      'underline': False},
-                                                      'href': None,
-                                                      'plain_text': request['Description'],
-                                                      'text': {'content': request['Description'],'link': None},
-                                                      'type': 'text'}],
-                                       'type': 'rich_text'},
-                 'Status': {'id': 'sHE%5E',
-                            'multi_select': [{'name': request['Status']}],
-                            'type': 'multi_select'},
-                 'Task': {'id': 'title',
-                          'title': [{'annotations': {'bold': False,
-                                                     'code': False,
-                                                     'color': 'default',
-                                                     'italic': False,
-                                                     'strikethrough': False,
-                                                     'underline': False},
-                                     'href': None,
-                                     'plain_text': request['Title'],
-                                     'text': {'content': request['Title'],'link': None},
-                                     'type': 'text'}],
-                          'type': 'title'}}
+                            'rich_text': [{'annotations': {'bold': False,
+                                                           'code': False,
+                                                           'color': 'default',
+                                                           'italic': False,
+                                                           'strikethrough': False,
+                                                           'underline': False},
+                                           'href': None,
+                                           'plain_text': request['Due date'],
+                                           'text': {'content': request['Due date'], 'link': None},
+                                           'type': 'text'}],
+                            'type': 'rich_text'},
+               'Short Description': {'id': 'DEgC',
+                                     'rich_text': [{'annotations': {'bold': False,
+                                                                    'code': False,
+                                                                    'color': 'default',
+                                                                    'italic': False,
+                                                                    'strikethrough': False,
+                                                                    'underline': False},
+                                                    'href': None,
+                                                    'plain_text': request['Description'],
+                                                    'text': {'content': request['Description'], 'link': None},
+                                                    'type': 'text'}],
+                                     'type': 'rich_text'},
+               'Status': {'id': 'sHE%5E',
+                          'multi_select': [{'name': request['Status']}],
+                          'type': 'multi_select'},
+               'Task': {'id': 'title',
+                        'title': [{'annotations': {'bold': False,
+                                                   'code': False,
+                                                   'color': 'default',
+                                                   'italic': False,
+                                                   'strikethrough': False,
+                                                   'underline': False},
+                                   'href': None,
+                                   'plain_text': request['Title'],
+                                   'text': {'content': request['Title'], 'link': None},
+                                   'type': 'text'}],
+                        'type': 'title'}}
 
     # To see the database schema notion.databases.retrieve('63cd54d3b2254b02b9f258c52e38400a')
-    notion.pages.create( parent={'database_id': '63cd54d3b2254b02b9f258c52e38400a'}, properties= payload)
+    notion.pages.create(
+        parent={'database_id': '63cd54d3b2254b02b9f258c52e38400a'}, properties=payload)
     return print(f'Task {request["Title"]} has been successfully added to Kanban with status: {request["Status"]} ✔ ')
--- menues.py
+++ menues.py
@@ -1,9 +1,12 @@
+import os
 import actions as ac
 from InquirerPy import inquirer
 from tabulate import tabulate
 from colorama import Back , Fore, Style
 
 def add_task_menu():
+    
+    '''Creates a new task'''
 
     t = inquirer.text(message="Add task title").execute()
     d = inquirer.text(message="Add task description").execute()
@@ -21,34 +24,54 @@ def add_task_menu():
     return
 
 
-def list_tasks_menu():
+
+
+def list_tasks_menu(): #Optimizar
+    
+    '''Calls the API in the actions module and returns it's results'''
+    
     print(Fore.GREEN + 'Loading your tasks this may take some time 🕐')
-    tasks_payload , task_ids_payload = ac.list_tasks_action()
+    tasks_payload , task_ids_payload = ac.list_tasks_action(showbar=True)
     h = ["Title","Short Description","Status","Due date"]
     print(tabulate(tasks_payload,headers=h,tablefmt="fancy_grid",numalign="center",stralign="center",showindex=True))
+        
+    
     return task_ids_payload
 
 
-def edit_task_menu(ls):
+def edit_task_menu(ls): #TODO
     task_name = inquirer.fuzzy(message="Select a task to edit (this will change the entire task)", choices=ls).execute()
     action = ac.edit_task_action(task_name)
-
-    r = {
-
-    }
-
     return print(f"Task {task_name} was edited successfully")
 
 
-def delete_task_menu():
-    ids = list_tasks_menu()
+tasks_payload , task_ids_payload = ac.list_tasks_action(showbar=False)
+
+def delete_task_menu(): #Optimizar
+    
+    '''Prints out all the tasks and asks the user which one does he want to delete'''
+
+    print(Fore.GREEN + 'Loading your tasks this may take some time 🕐')
+    h = ["Title","Short Description","Status","Due date"]
+    print(tabulate(tasks_payload,headers=h,tablefmt="fancy_grid",numalign="center",stralign="center",showindex=True))
+    
+    '''Makes the user choose a task to delete'''
+    
     task_id = inquirer.number(message="Select a task number to delete ❌ ").execute()
-    ac.delete_task_action(ids[int(task_id)])
+    ac.delete_task_action(task_ids_payload[int(task_id)])
     print(f"Task number: {task_id} was deleted successfully")
+    
+    '''Asks the user if he wants to clear the output'''
+    
+    clear = inquirer.confirm(message="clear ?").execute()
+    
+    if clear:
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
     return
 
 
-def set_status_menu():
+def set_status_menu(): #TODO
     ls = ac.list_tasks_action()
     task_name = inquirer.rawlist(message="Choose a task to change it's state", choices=ls).execute()
     new_status = inquirer.rawlist(message="Select new status", choices=["Backlog", "To do", "Doing", "Done"])
