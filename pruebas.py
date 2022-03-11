b26ed35d904b1b547b783ad3be052001bbdc5c4f
Facundo <57848306+facundomartinezabeldano@users.noreply.github.com>
2022-03-11 10:33:05 -0300

    Woopsie
    Didn't mean to push this

--- pruebas.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def clean_screen_and_print_logo():
-    with open(os.path.join("src","userdata.json"), "r", encoding="utf8") as f:
-        for line in f:
-            print(line)
-    return
-
-clean_screen_and_print_logo()
